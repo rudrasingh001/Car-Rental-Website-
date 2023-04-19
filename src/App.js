f7eb3fa304dc95844ddf4bbe86467a45b8755d27
Rudra Singh <rs7171507@gmail.com>
2023-04-19 23:03:05 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/dist/styles.css";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Navbar from "../src/components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Models from "./Pages/Models";
+import TestimonialsPage from "./Pages/TestimonialsPage";
+
+import Contact from "./Pages/Contact";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="models" element={<Models />} />
+        <Route path="testimonials" element={<TestimonialsPage />} />
+        
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Pages/About.jsx
@@ -0,0 +1,78 @@
+import Footer from "../components/Footer";
+import HeroPages from "../components/HeroPages";
+import PlanTrip from "../components/PlanTrip";
+import AboutMain from "../images/about/about-main1.jpg";
+import Box1 from "../images/about/icon1.png";
+import Box2 from "../images/about/icon2.png";
+import Box3 from "../images/about/icon3.png";
+
+function About() {
+  return (
+    <>
+      <section className="about-page">
+        <HeroPages name="About" />
+        <div className="container">
+          <div className="about-main">
+            <img
+              className="about-main__img"
+              src={AboutMain}
+              alt="car-renting"
+            />
+            <div className="about-main__text">
+              <h3>About Company</h3>
+              <h2>You start the engine and your adventure begins</h2>
+              <p>
+              At Rudra Car Rental Services, we believe that renting a car should
+              be an effortless and enjoyable experience. That's why we offer a 
+              wide selection of high-quality vehicles to choose from, along with
+              competitive pricing and exceptional customer service. Plus, we're committed to sustainability, with
+              a fleet of fuel-efficient and hybrid vehicles that reduce our carbon
+              footprint. Whether you're renting for business or pleasure, we're here
+              to make your trip a success.
+              </p>
+              <div className="about-main__text__icons">
+                <div className="about-main__text__icons__box">
+                  <img src={Box1} alt="car-icon" />
+                  <span>
+                    <h4>20</h4>
+                    <p>Car Types</p>
+                  </span>
+                </div>
+                <div className="about-main__text__icons__box">
+                  <img src={Box2} alt="car-icon" />
+                  <span>
+                    <h4>85</h4>
+                    <p>Rental Outlets</p>
+                  </span>
+                </div>
+                <div className="about-main__text__icons__box">
+                  <img src={Box3} alt="car-icon" className="last-fk" />
+                  <span>
+                    <h4>75</h4>
+                    <p>Repair Shop</p>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <PlanTrip />
+        </div>
+      </section>
+      <div className="book-banner">
+        <div className="book-banner__overlay"></div>
+        <div className="container">
+          <div className="text-content">
+            <h2>Book a car by getting in touch with us</h2>
+            <span>
+              <i className="fa-solid fa-phone"></i>
+              <h3>(123) 456-7869</h3>
+            </span>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default About;
